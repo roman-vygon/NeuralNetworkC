a5f26c8f693a30d1b52566dff372a1abbb28d625
roman-vygon <roman.vygon@gmail.com>
2019-03-03 20:39:44 +0700

    initial

---
+++ Interface/NeuralNetworkInterface/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace NeuralNetworkInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(80, 80);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Определить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(24, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ответ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(133, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Interface/NeuralNetworkInterface/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace NeuralNetworkInterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool mouse_down = false;
+        private Bitmap number = new Bitmap(80, 80);
+        private Bitmap clear = new Bitmap(80, 80);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            int[] a = new int[50000];
+            /*  int width = Convert.ToInt32(panel1.Width);
+              int height = Convert.ToInt32(panel1.Height);
+              Bitmap bmp = new Bitmap(width, height);
+              panel1.DrawToBitmap(bmp, new Rectangle(0, 0, width, height));
+
+              bmp.Save(path+@"\CurrentNumber.png", ImageFormat.Jpeg);
+
+
+          Image newImage = Image.FromFile(path + @"\CurrentNumber.png");
+          Bitmap b1 = new Bitmap(newImage);*/
+
+            for (int j = 0; j < 20; j ++)
+            {
+                for (int k = 0; k < 20; k++)
+                {
+                    Color col = number.GetPixel(j*4, k*4);
+                    if (col.Name != "0")
+                        a[k * 20 + j] = 1;
+                    else
+                        a[k * 20 + j] = 0;
+                }
+            }
+            label2.Text = path;
+           
+            string FileName = path + @"\input1.txt";
+            System.IO.StreamWriter file = new System.IO.StreamWriter(FileName, false, System.Text.Encoding.Default);
+            file.Write("");
+            file.Close();
+            System.IO.StreamWriter file1 = new System.IO.StreamWriter(FileName, true, System.Text.Encoding.Default);
+           
+            for (int j = 0; j < 400; j++)
+            {
+                if (j % 20 == 0 && j != 0)
+                    file1.WriteLine();
+                if (a[j] == 1)
+                    file1.Write("1 ");
+                else
+                    file1.Write("0 ");
+            }
+
+          Process.Start(path + @"\NeuralNetwork.exe");
+            Thread.Sleep(1000);
+            file.Close();
+            file1.Close();
+            string text = System.IO.File.ReadAllText(path + @"\output.txt");
+            label2.Text = text;
+            clear = new Bitmap(clear, panel1.Size);
+            panel1.BackgroundImage = clear;
+            number = new Bitmap(80, 80);
+            
+        
+    }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_down = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_down = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouse_down)
+            {
+                number = new Bitmap(number, panel1.Size);
+                panel1.BackgroundImage = number;
+                if (e.X - 2 >= 0 && e.X + 2 < number.Width && e.Y - 2 >= 0 && e.Y + 2 < number.Height)
+                {
+                    for (int i = -2; i <= 2; i++)
+                        for (int j = -2; j <= 2; j++)
+                            number.SetPixel(e.X + i, e.Y + j, Color.Black);
+
+                }
+            }
+
+        }
+    }
+}
